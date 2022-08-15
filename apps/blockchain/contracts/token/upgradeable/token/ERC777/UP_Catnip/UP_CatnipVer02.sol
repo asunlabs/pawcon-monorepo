e528fc75cfc14698d2f6edc85d9e045887aafd0a
designerasun <asuntheawesome@gmail.com>
2022-08-15 20:15:26 +0900

    ENH:add stake-related events, fix staking logics

--- apps/blockchain/contracts/token/upgradeable/token/ERC777/UP_Catnip/UP_CatnipVer02.sol
+++ apps/blockchain/contracts/token/upgradeable/token/ERC777/UP_Catnip/UP_CatnipVer02.sol
@@ -2,19 +2,15 @@
 pragma solidity ^0.8.15;
 
 import "./UP_Catnip.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/TokenTimelockUpgradeable.sol";
 
-contract UP_CatnipVer02 is UP_Catnip {
-    uint256 public supplyLimit;
+contract UP_CatnipVer02 is UP_Catnip, IERC777RecipientUpgradeable {
     uint256 public releaseTime;
     mapping(address => uint256) public beneficiaries; // stake holder and balance
 
     event ReleaseTime(uint256 _releaseTime);
-
-    function initialize() public override reinitializer(2) {
-        UP_Catnip.initialize();
-        supplyLimit = 1000000000 * uint256(10**decimals());
-    }
+    event StakedTokenReceived(address operator, address from, address to, uint256 amount, bytes userData, bytes operatorData);
+    event Stake(address staker, uint256 stakeAmount);
+    event UnStake(address unStaker, uint256 unStakeAmount);
 
     function catnipVersion() external pure returns (uint256) {
         return 2;
@@ -38,20 +34,40 @@ contract UP_CatnipVer02 is UP_Catnip {
         emit ReleaseTime(releaseTime);
     }
 
-    function stake(IERC777Upgradeable _token, uint256 _amount) public {
-        require(_token.balanceOf(msg.sender) > _amount);
-        _token.authorizeOperator(address(this));
+    function tokensReceived(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) public {
+        emit StakedTokenReceived(operator, from, to, amount, userData, operatorData);
+    }
+
+    function stake(uint256 _amount) public {
+        IERC777Upgradeable catnip = IERC777Upgradeable(address(this));
+        require(catnip.balanceOf(msg.sender) > _amount, "Should have catnips to stake");
 
         // token holder should authorize this contract as an operator
         setReleaseTime();
         beneficiaries[msg.sender] += _amount;
-        _token.send(address(this), _amount, "");
+
+        /**
+         * TODO ERC1820 implementation requires
+         * For this contract to be able to receive ERC777, it needs to implement ERC1820.
+         */
+        catnip.operatorSend(msg.sender, address(this), _amount, "", "");
+        emit Stake(msg.sender, _amount);
     }
 
-    function unStake(IERC777Upgradeable _token) public {
+    function unStake() public {
+        IERC777Upgradeable catnip = IERC777Upgradeable(address(this));
         require(releaseTime < block.timestamp, "Staking hasn't finished");
+
         beneficiaries[msg.sender] = 0;
-        uint256 unStakeAmount = _token.balanceOf(address(this));
-        _token.operatorSend(address(this), msg.sender, unStakeAmount, "", "");
+        uint256 unStakeAmount = catnip.balanceOf(msg.sender);
+        catnip.send(msg.sender, unStakeAmount, "");
+        emit UnStake(msg.sender, unStakeAmount);
     }
 }
