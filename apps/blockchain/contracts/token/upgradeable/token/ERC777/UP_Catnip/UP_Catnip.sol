89d9f07fe3ee253159f16c947aae2bb333630ce9
designerasun <asuntheawesome@gmail.com>
2022-08-15 20:14:49 +0900

    CLN:update disableInitializer comment, set supply limit in proxy

--- apps/blockchain/contracts/token/upgradeable/token/ERC777/UP_Catnip/UP_Catnip.sol
+++ apps/blockchain/contracts/token/upgradeable/token/ERC777/UP_Catnip/UP_Catnip.sol
@@ -8,9 +8,18 @@ import "@openzeppelin/contracts-upgradeable/token/ERC777/ERC777Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /// @author @developerasun
-/// @notice Upgradeable ERC777 Catnip token. Governance in PawCon project
+/// @notice Upgradeable ERC777 Catnip token. Governance token in PawCon project
 /// @dev Proxy, UUPS
 contract UP_Catnip is Initializable, ERC777Upgradeable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 public supplyLimit;
+
+    /**
+     * *_disableInitializers
+     * @dev Locks the contract, preventing any future reinitialization. This cannot be part of an initializer call.
+     * Calling this in the constructor of a contract will prevent that contract from being initialized or reinitialized
+     * to any version. It is recommended to use this to lock implementation contracts that are designed to be called
+     * through proxies.
+     */
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -20,6 +29,11 @@ contract UP_Catnip is Initializable, ERC777Upgradeable, UUPSUpgradeable, Ownable
         __ERC777_init("UP_Catnip", "CN", new address[](0));
         __UUPSUpgradeable_init();
         __Ownable_init();
+        __Setup_init();
+    }
+
+    function __Setup_init() private {
+        supplyLimit = 1000000000 * uint256(10**decimals());
     }
 
     /**
