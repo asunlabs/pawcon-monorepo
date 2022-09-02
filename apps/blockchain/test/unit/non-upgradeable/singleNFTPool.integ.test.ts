96ff6559a2419b22e86bd794c97b6d2cb5eeaa43
designerasun <asuntheawesome@gmail.com>
2022-09-02 21:15:42 +0900

    TST:nft staking reward test cases added

--- apps/blockchain/test/unit/non-upgradeable/singleNFTPool.integ.test.ts
+++ apps/blockchain/test/unit/non-upgradeable/singleNFTPool.integ.test.ts
@@ -9,19 +9,25 @@ import { useSnapshotForReset } from "../../../scripts/hooks/useNetworkHelper";
 import { useUUPSDeployer } from "../../../scripts/hooks/useUpgradeDeployer";
 
 const PREFIX = "integ-singleNFTPool";
+const rewardAmount = ethers.utils.parseEther("100");
 
 async function useFixture() {
   const { contract } = await deploy("SingleNFTPool");
   return { contract };
 }
 
-async function useUpgradeDeployer() {
+async function useUpgradeDeployerForNFT() {
   return await useUUPSDeployer("CuriousPawoneer");
+}
+
+async function useUpgradeDeployerForFT() {
+  return await useUUPSDeployer("Catnip");
 }
 
 let upgraded: Contract;
 let tokenContract: Contract;
 let poolContract: Contract;
+let catnipVer02: Contract;
 
 let _owner: SignerWithAddress;
 let _recipient: SignerWithAddress;
@@ -30,10 +36,12 @@ describe(`${PREFIX}-NFT-transfer`, function TestNFTTransfer() {
   this.beforeEach("Should reset blockchain", async function TestReset() {
     await useSnapshotForReset();
 
+    /// @dev Staking pool setup
     const { contract } = await useFixture();
     poolContract = contract;
 
-    const _tokenContract = await useUpgradeDeployer();
+    /// @dev ERC721 CuriousPawoneer setup
+    const _tokenContract = await useUpgradeDeployerForNFT();
     tokenContract = _tokenContract.contract;
     upgraded = await upgrades.upgradeProxy(tokenContract.address, await ethers.getContractFactory("CuriousPawoneerVer02"));
 
@@ -41,9 +49,18 @@ describe(`${PREFIX}-NFT-transfer`, function TestNFTTransfer() {
 
     _owner = owner;
     _recipient = recipient;
+
+    /// @dev ERC777 Catnip setup
+    const _catnip = await useUpgradeDeployerForFT();
+    const catnipProxy = _catnip.contract;
+    const catnipVer02Factory = await ethers.getContractFactory("CatnipVer02");
+
+    catnipVer02 = await upgrades.upgradeProxy(catnipProxy.address, catnipVer02Factory, {
+      kind: "uups",
+    });
   });
 
-  it.only("Should receive NFT", async function TestNFTReceival() {
+  it("Should receive NFT", async function TestNFTReceival() {
     await upgraded.connect(_owner).__Ver02Setup_init();
 
     await upgraded.connect(_owner).setWhitelist(_owner.address);
@@ -56,7 +73,7 @@ describe(`${PREFIX}-NFT-transfer`, function TestNFTTransfer() {
     expect(await upgraded.ownerOf(tokenId)).to.equal(poolContract.address);
   });
 
-  it.only("Should stake NFT", async function TestNFTStaking() {
+  it("Should stake NFT", async function TestNFTStaking() {
     await upgraded.connect(_owner).__Ver02Setup_init();
     await upgraded.connect(_owner).safeMint(_owner.address, { value: ethers.utils.parseEther("0.0001") });
 
@@ -66,7 +83,7 @@ describe(`${PREFIX}-NFT-transfer`, function TestNFTTransfer() {
 
     /// @dev token transfer approval must be done
     await upgraded.connect(_owner).approve(poolContract.address, tokenId);
-    await expect(poolContract.connect(_owner).stake(upgraded.address, tokenId)).not.to.reverted;
+    await expect(poolContract.connect(_owner).stake(catnipVer02.address, upgraded.address, tokenId)).not.to.reverted;
 
     expect(await poolContract.stakedTokenlist(_owner.address, tokenId)).not.to.reverted;
     expect(await poolContract.stakedTokenlist(_owner.address, tokenId))
@@ -77,14 +94,14 @@ describe(`${PREFIX}-NFT-transfer`, function TestNFTTransfer() {
     console.log(await poolContract.stakedTokenlist(_owner.address, tokenId));
   });
 
-  it.only("Should unstake NFT", async function TestNFTUnstaking() {
+  it("Should unstake NFT", async function TestNFTUnstaking() {
     await upgraded.__Ver02Setup_init();
     await upgraded.connect(_owner).safeMint(_owner.address, { value: ethers.utils.parseEther("0.0001") });
 
     const tokenId = 0;
 
     await upgraded.connect(_owner).approve(poolContract.address, tokenId);
-    await poolContract.connect(_owner).stake(upgraded.address, tokenId);
+    await poolContract.connect(_owner).stake(catnipVer02.address, upgraded.address, tokenId);
 
     /// @dev staking is one year
     await expect(poolContract.unstake(upgraded.address, tokenId)).to.be.revertedWith("Staking not finished");
@@ -94,5 +111,22 @@ describe(`${PREFIX}-NFT-transfer`, function TestNFTTransfer() {
 
     await expect(poolContract.connect(_owner).unstake(upgraded.address, tokenId)).not.to.be.reverted;
     expect(await upgraded.ownerOf(tokenId)).to.equal(_owner.address);
+  });
+
+  it("Should calculate staking reward", async function TestRewardCalculation() {
+    expect(await poolContract.calculateReward()).to.equal(rewardAmount);
+  });
+
+  it("Should reward NFT staker", async function TestNFTStakingReward() {
+    /// Upgradeable NFT
+    await upgraded.__Ver02Setup_init();
+    await upgraded.connect(_owner).safeMint(_owner.address, { value: ethers.utils.parseEther("0.0001") });
+
+    const tokenId = 0;
+
+    await upgraded.connect(_owner).approve(poolContract.address, tokenId);
+    await poolContract.connect(_owner).stake(catnipVer02.address, upgraded.address, tokenId);
+
+    expect(await catnipVer02.balanceOf(_owner.address)).to.equal(rewardAmount);
   });
 });
