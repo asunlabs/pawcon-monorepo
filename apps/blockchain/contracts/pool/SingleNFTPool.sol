81eafbcd0319379ac75193d21299be25392fe801
designerasun <asuntheawesome@gmail.com>
2022-09-02 18:41:22 +0900

    ENH:split staking/unstaking logic in contracts

--- apps/blockchain/contracts/pool/SingleNFTPool.sol
+++ apps/blockchain/contracts/pool/SingleNFTPool.sol
@@ -15,18 +15,62 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradeable.sol";
 
 contract SingleNFTPool is IERC721ReceiverUpgradeable, IERC721Receiver {
-    /// @dev holder address => (tokenId => staked amount)
-    mapping(address => mapping(uint256 => uint256)) public stakerList;
+    /// @dev staker => tokenId => StakedNFT
+    mapping(address => mapping(uint256 => StakedNFT)) public stakedTokenlist;
+    mapping(address => mapping(uint256 => bool)) public isStaker;
 
-    function rewardStaker() public {}
+    event NFTStaked(address staker, uint256 tokenId, uint256 stakedAt);
+    event NFTUnstaked(address stakingContract, uint256 tokenId, uint256 unstakedAt);
+    event ReceivedERC721Token(address operator, address from, uint256 tokenId, bytes data);
 
-    // solhint-disable-next-line
+    struct StakedNFT {
+        address owner;
+        uint256 tokenId;
+        uint256 stakedAt;
+        uint256 unstakableAt;
+    }
+
+    /// @dev now contract receivable ERC721
     function onERC721Received(
         address operator,
         address from,
         uint256 tokenId,
         bytes calldata data
-    ) external pure override(IERC721Receiver, IERC721ReceiverUpgradeable) returns (bytes4) {
+    ) external override(IERC721Receiver, IERC721ReceiverUpgradeable) returns (bytes4) {
+        emit ReceivedERC721Token(operator, from, tokenId, data);
         return IERC721Receiver.onERC721Received.selector;
+    }
+
+    function stake(address _curiousPawoneer, uint256 _tokenId) external {
+        IERC721Upgradeable curiousPawoneer = IERC721Upgradeable(_curiousPawoneer);
+        require(curiousPawoneer.ownerOf(_tokenId) == msg.sender, "Only owner");
+
+        /// @dev note that token transfer approval should be done in user sider(front side)
+        curiousPawoneer.safeTransferFrom(msg.sender, address(this), _tokenId, "");
+
+        uint256 oneYear = 4 weeks * 12;
+
+        StakedNFT memory stakedNFT = StakedNFT({
+            owner: msg.sender,
+            tokenId: _tokenId,
+            stakedAt: block.timestamp,
+            unstakableAt: block.timestamp + oneYear
+        });
+
+        stakedTokenlist[msg.sender][_tokenId] = stakedNFT;
+        isStaker[msg.sender][_tokenId] = true;
+
+        emit NFTStaked(msg.sender, _tokenId, block.timestamp);
+    }
+
+    function unstake(address _curiousPawoneer, uint256 _tokenId) external {
+        IERC721Upgradeable curiousPawoneer = IERC721Upgradeable(_curiousPawoneer);
+
+        require(isStaker[msg.sender][_tokenId] == true, "Token not staked");
+        require(stakedTokenlist[msg.sender][_tokenId].unstakableAt < block.timestamp, "Staking not finished");
+
+        curiousPawoneer.safeTransferFrom(address(this), msg.sender, _tokenId, "");
+
+        emit NFTUnstaked(address(this), _tokenId, block.timestamp);
     }
 }
