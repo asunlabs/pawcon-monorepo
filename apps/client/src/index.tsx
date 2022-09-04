af0ea22cac91e0a2fdfbe23722b7b2b03e46a0e7
designerasun <asuntheawesome@gmail.com>
2022-09-05 00:15:51 +0900

    ENH:set react router for app

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './app/store'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Swap } from './features/swap/Swap';
+import { Mint } from './features/mint/Mint';
 
-const container = document.getElementById('root')!
-const root = createRoot(container)
+const container = document.getElementById('root')!;
+const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="mint" element={<Mint />} />
+                <Route path="swap" element={<Swap />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
