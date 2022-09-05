e11d0b4fa0e48782d14949bf9f8aef441b459637
designerasun <asuntheawesome@gmail.com>
2022-09-05 22:13:58 +0900

    ENH:set toggle context provider

--- apps/client/src/features/layout/navigation/Navigation.tsx
+++ apps/client/src/features/layout/navigation/Navigation.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import './Navigation.css';
 import logo from './pawcon-logo.svg';
 import { Buttom } from '../button/Button';
-import { breakpoint } from '../../../app/type';
 
 function handleSIWE() {
     console.log('auth 1');
@@ -12,19 +11,41 @@ function handleWalletConnect() {
     console.log('auth 2');
 }
 
-function TriggerBar() {
+const ToggleContext = React.createContext(false);
+
+interface IToggleContextProviderProps {
+    children?: React.ReactNode;
+}
+
+function ToggleContextProvider({ children }: IToggleContextProviderProps) {
+    const [toggle, setToggle] = React.useState(false);
+
     return (
-        <div className="navigationBar" id="trigger">
-            <span id="logotype">PawCon</span>
-            <img src={logo} alt="logo" />
-            <img src={logo} alt="burger" />
-        </div>
+        <ToggleContext.Provider value={toggle}>
+            <div className="navigationBar" id="trigger">
+                <span id="logotype">PawCon</span>
+                <img src={logo} alt="logo" loading="lazy" />
+                <span
+                    id="menuTrigger"
+                    onClick={() => {
+                        setToggle(!toggle);
+                    }}
+                >
+                    {/*
+                     * &#9776; => ≡,  &#10005 => X
+                     */}
+                    {toggle ? <span>&#9776;</span> : <span>&#10005;</span>}
+                </span>
+            </div>
+            {children}
+        </ToggleContext.Provider>
     );
 }
 
 function MenuBar() {
+    const toggle = React.useContext(ToggleContext);
     return (
-        <ul className="navigationBar" id="menu">
+        <ul className="navigationBar" id={toggle ? 'menu' : 'disabled'}>
             <li className="menuItem">
                 <a href="#asunMint">AsunMint</a>
             </li>
@@ -49,13 +70,12 @@ function MenuBar() {
     );
 }
 
-export interface INavigationProps {}
-
-export function Navigation(props: INavigationProps) {
+export function Navigation() {
     return (
         <nav id="nav">
-            <TriggerBar />
-            <MenuBar />
+            <ToggleContextProvider>
+                <MenuBar />
+            </ToggleContextProvider>
         </nav>
     );
 }
