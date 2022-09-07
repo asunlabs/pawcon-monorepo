0761d00d6cb87538b484da5248a37000dfad1b23
designerasun <asuntheawesome@gmail.com>
2022-09-07 23:47:24 +0900

    CLN:update banner, card component

--- apps/client/src/features/layout/card/Card.tsx
+++ apps/client/src/features/layout/card/Card.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import { Buttom } from '../button/Button';
 import './Card.css';
 
@@ -8,23 +9,43 @@ export interface ICardProps {
     image?: string;
     bid?: string;
     text?: string;
+    linkTo?: string;
 }
 
-export function Card({ description, title, image, bid, text }: ICardProps) {
+export function Card({
+    description,
+    title,
+    image,
+    bid,
+    text,
+    linkTo,
+}: ICardProps) {
     return (
         <div className="card">
-            <img
-                className="cardImage"
-                src={image}
-                alt="gallery card"
-                loading="lazy"
-            />
+            {image !== undefined ? (
+                <img
+                    className="cardImage"
+                    src={image}
+                    alt="card"
+                    loading="lazy"
+                />
+            ) : (
+                ''
+            )}
+
             <div className="cardDetails">
                 <h3 className="title">{title}</h3>
-                <span className="bid">{bid} ETH</span>
+                <span className="bid">{bid}</span>
                 <p className="description">{description}</p>
             </div>
-            <Buttom text={text} />
+
+            {linkTo !== undefined ? (
+                <Link to={linkTo}>
+                    <Buttom text={text} />{' '}
+                </Link>
+            ) : (
+                <Buttom text={text} />
+            )}
         </div>
     );
 }
