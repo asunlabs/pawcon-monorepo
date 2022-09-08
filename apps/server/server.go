eaae901b775a4cd5cfab54ee1839bf77523b7573
designerasun <asuntheawesome@gmail.com>
2022-09-08 17:10:39 +0900

    ENH:fiber config, static asset, router added

--- apps/server/server.go
+++ apps/server/server.go
@@ -1,15 +1,65 @@
+/*
+       /$$                               /$$
+      | $$                              | $$
+  /$$$$$$$  /$$$$$$  /$$    /$$ /$$$$$$ | $$  /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$   /$$$$$$$ /$$   /$$ /$$$$$$$
+ /$$__  $$ /$$__  $$|  $$  /$$//$$__  $$| $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$|____  $$ /$$_____/| $$  | $$| $$__  $$
+| $$  | $$| $$$$$$$$ \  $$/$$/| $$$$$$$$| $$| $$  \ $$| $$  \ $$| $$$$$$$$| $$  \__/ /$$$$$$$|  $$$$$$ | $$  | $$| $$  \ $$
+| $$  | $$| $$_____/  \  $$$/ | $$_____/| $$| $$  | $$| $$  | $$| $$_____/| $$      /$$__  $$ \____  $$| $$  | $$| $$  | $$
+|  $$$$$$$|  $$$$$$$   \  $/  |  $$$$$$$| $$|  $$$$$$/| $$$$$$$/|  $$$$$$$| $$     |  $$$$$$$ /$$$$$$$/|  $$$$$$/| $$  | $$
+ \_______/ \_______/    \_/    \_______/|__/ \______/ | $$____/  \_______/|__/      \_______/|_______/  \______/ |__/  |__/
+                                                      | $$
+                                                      | $$
+                                                      |__/
+*/
+
 package main
 
 import (
+	"log"
+	"time"
 	"github.com/gofiber/fiber/v2"
-	"github.com/asunlabs/pawcon-monorepo/server/src/feature/api"
+	"github.com/asunlabs/pawcon-monorepo/server/src/feature/auth"
 )
 
-
 func main() {
-	app := fiber.New()
 	
-	app.Get("/", api.CollectionHandler)
+	// ================ App config ================ //
+	app := fiber.New(fiber.Config{
+		AppName: "PawCon server v0.3.0",
+		CaseSensitive: false,
+		StrictRouting: false,
+		Immutable: false,
+		GETOnly: false,
+	})
+	// ================ App config ================ //
+	
+	// ================ API ================ //
+	/// @dev /api => virtual path prefix
+	app.Static("/api", "./public", fiber.Static{
+		Compress: true, // CPU minimization
+		CacheDuration: 10 * time.Second,
+		MaxAge: 3600, // 5 mins
+	})
+	// ================ API ================ //
+	
+	// ================ Routers ================ //
+	app.Group("/auth", auth.AuthHandler)
+	// ================ Routers ================ //
 
-	app.Listen(":3000")
+	app.Get("/", func(c *fiber.Ctx) error {
+		type PawConServerGuide struct {
+			Routers []string `json:"routers"`
+			Handlers uint32 `json:"numberOfHandlers"`
+		} 
+
+		pawconServerGuide := PawConServerGuide{
+			Routers: []string {"/api, /auth"},
+			Handlers: app.HandlersCount(),
+		}
+		return c.JSON(pawconServerGuide)
+	})
+
+	
+
+	log.Fatal(app.Listen(":3000"))
 }
