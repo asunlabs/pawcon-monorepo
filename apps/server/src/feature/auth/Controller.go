9076b8970f96f5a877b1828199a32bfc4fefe322
designerasun <asuntheawesome@gmail.com>
2022-09-10 23:27:09 +0900

    EHN: set auth module proto with gorm

--- apps/server/src/feature/auth/Controller.go
+++ apps/server/src/feature/auth/Controller.go
@@ -2,6 +2,8 @@ package auth
 
 import "github.com/gofiber/fiber/v2"
 
-func AuthHandler(c *fiber.Ctx) error {
+func Default(c *fiber.Ctx) error {
 	return c.SendString("JWT, Oauth will be set")
 }
+
+// TODO set CRUD handlers
