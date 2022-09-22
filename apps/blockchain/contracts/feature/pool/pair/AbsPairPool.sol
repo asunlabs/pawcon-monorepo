e2f8a3067c95c238b372d7838d77c95c92223634
designerasun <asuntheawesome@gmail.com>
2022-09-22 22:49:52 +0900

    ENH:add yield farming logics

--- apps/blockchain/contracts/feature/pool/pair/AbsPairPool.sol
+++ apps/blockchain/contracts/feature/pool/pair/AbsPairPool.sol
@@ -4,10 +4,23 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC777Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/interfaces/IERC1820RegistryUpgradeable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+import "hardhat/console.sol";
+
+interface ICatnip {
+    function mint(
+        address account,
+        uint256 amount,
+        bytes memory userData,
+        bytes memory operatorData
+    ) external;
+}
 
 abstract contract AbsPairPool {
-    IERC1820RegistryUpgradeable _ERC1820_REGISTRY;
+    using SafeMath for uint256;
 
+    IERC1820RegistryUpgradeable _ERC1820_REGISTRY;
     mapping(address => Liquidity) public liquidityBalance;
 
     struct Liquidity {
@@ -15,6 +28,9 @@ abstract contract AbsPairPool {
         uint256 leftTokenAmount;
         uint256 rightTokenAmount;
     }
+
+    event LiquidityEthCatnip(uint256 ethAmount, uint256 catnipAmount);
+    event LiquidityReward(address provider, uint256 rewardAmount);
 
     constructor() {
         _ERC1820_REGISTRY = IERC1820RegistryUpgradeable(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
@@ -33,6 +49,7 @@ abstract contract AbsPairPool {
         Liquidity memory liq = Liquidity({provider: msg.sender, leftTokenAmount: catnip.balanceOf(msg.sender), rightTokenAmount: msg.value});
 
         liquidityBalance[msg.sender] = liq;
+        emit LiquidityEthCatnip(msg.value, catnipAmount);
     }
 
     // pool type: (ETH, ERC20)
@@ -45,12 +62,29 @@ abstract contract AbsPairPool {
         require(erc20.balanceOf(provider) > tokenAmount);
         require(address(provider).balance > msg.value);
 
-        // should receive ERC20 and ether
+        // ! NOT DONE
     }
 
-    function _calcualteReward() internal virtual {}
+    // reward type: (ETH, ERC777)
+    function _calculateReward(address provider) internal virtual returns (uint256) {
+        uint256 left = liquidityBalance[provider].leftTokenAmount;
+        uint256 right = liquidityBalance[provider].rightTokenAmount;
+        uint256 DIVIDER = 2;
 
-    function _rewardLiquidityProvider() internal virtual {}
+        uint256 sum = left.add(right);
+        (bool result, uint256 rewardAmount) = sum.tryDiv(DIVIDER);
+        require(result == true, "Calculation failed");
+        console.log("rewardAmount: ", rewardAmount);
+
+        return rewardAmount;
+    }
+
+    function _rewardLiquidityProvider(address provider, address _catnip) internal virtual {
+        uint256 rewardAmount = _calculateReward(provider);
+        ICatnip catnip = ICatnip(_catnip);
+        catnip.mint(provider, rewardAmount, "", "");
+        emit LiquidityReward(provider, rewardAmount);
+    }
 
     // =================== ERC777 receivability ================ //
     /// @dev prevent token being locked (1)
--- apps/blockchain/contracts/feature/pool/pair/PairPool.sol
+++ apps/blockchain/contracts/feature/pool/pair/PairPool.sol
@@ -8,8 +8,10 @@ import "./AbsPairPool.sol";
  * 2) Define a pool factory contract
  */
 contract PairPool is AbsPairPool {
+    using SafeMath for uint256;
+
     function provideEthCatnipLiquidity(address _catnip, uint256 catnipAmount) public payable virtual {
-        _provideLiquidity(_catnip, catnipAmount);
+        super._provideLiquidity(_catnip, catnipAmount);
     }
 
     function getEthCatnipPairAmount(address provider) public view returns (uint256, uint256) {
@@ -17,5 +19,18 @@ contract PairPool is AbsPairPool {
         uint256 right = liquidityBalance[provider].rightTokenAmount;
 
         return (left, right);
+    }
+
+    /**
+     * (ETH, Catnip) yield farming reward: (whole amount of left & right tokens / 2)
+     */
+    function calculateReward(address provider) public returns (uint256) {
+        uint256 rewardAmount = super._calculateReward(provider);
+        console.log("pair pool reward amount: ", rewardAmount);
+        return rewardAmount;
+    }
+
+    function rewardLiquidityProvider(address provider, address _catnip) public {
+        super._rewardLiquidityProvider(provider, _catnip);
     }
 }
