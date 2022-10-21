5ce0bb0d128b56182a7fcebe1277b4efe18c3aea
designerasun <asuntheawesome@gmail.com>
2022-10-21 22:26:53 +0900

    ENH:split db connection per app type

--- apps/server/src/app/database/database.go
+++ apps/server/src/app/database/database.go
@@ -1,11 +1,13 @@
 package database
 
 import (
+	"log"
+	"os"
+
 	"github.com/fatih/color"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"os"
 )
 
 var (
@@ -13,16 +15,41 @@ var (
 	Conn *gorm.DB
 )
 
-func Connect(schema []interface{}) {
+
+func Connect(schema []interface{}, _appType string)  {
+	appType := []string{"production", "develop", "test"}
+
+	isValidType := false
+
+	for _, v := range appType {
+		if v == _appType {
+			isValidType = true
+			break
+		}
+	}
+
+	if isValidType {
+		switch _appType {
+			case "production":
+				_connect(schema)
+			case "develop":
+				_connect(schema)
+			case "test":
+				_connectForTest(schema)
+		}
+	}
+
+	if !isValidType {
+		log.Fatal("Database: Invalid app type")
+	}
+}
+
+func _connect(schema []interface{}) {
 	db, err := gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
 	Conn = db
 
 	if err != nil {
 		color.Red("Connection failed with err code: %d", fiber.StatusInternalServerError)
-		/*
-			Exit causes the current program to exit with the given status code.
-			Conventionally, code zero indicates success, non-zero an error.
-		*/
 		os.Exit(1)
 	} else {
 		// iterate array in Go: for ~ range
@@ -30,5 +57,20 @@ func Connect(schema []interface{}) {
 			db.AutoMigrate(v)
 		}
 		color.Green("Schema auto-migrated, DB connected")
+	}
+}
+
+func _connectForTest(schema []interface{}) {
+	testDB, err := gorm.Open(sqlite.Open("appTest.db"), &gorm.Config{})
+	Conn = testDB
+
+	if err != nil {
+		color.Red(err.Error())
+		os.Exit(1)
+	}
+
+	for _, v := range schema {
+		testDB.AutoMigrate(v)
+		color.Green("Test DB connected")
 	}
 }
